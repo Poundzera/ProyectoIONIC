2a4bf235861a58ead9622a1cd0fdab090640082c
CETECOM_AVARAS <cetecom_avaras@duoc.cl>
2024-08-30 11:09:22 -0400

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,33 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private router: Router) {}
+
+    usuario = {
+      username :"",
+      password :"",
+    }
+    mensaje ="";
+
+    validar() {
+      if (this.usuario.username.length >= 3) {
+        if (this.usuario.password.length == 4){
+          let navigationExtras: NavigationExtras = {
+            state: {
+              username: this.usuario.username,
+              password: this.usuario.password,
+            },
+          };
+         // this.router.navigate (['/inicio'],navigationExtras);
+        }else{
+          console.log('Contraseña incorrecta');
+          this.mensaje = 'Contraseña no cumple';
+        }
+      
+      }else{
+        console.log('Usuario incorrecto');
+        this.mensaje = 'Usuario no cumple';
+      }
+    }
 
 }
